573bbc07b88929907eb03b04f217a9cabf6ffeb2
rRPaulina <161756316+rRPaulina@users.noreply.github.com>
2026-09-10 00:19:39 -0600

    Revert "Implement loyalty discount tiers"

--- store.py
+++ store.py
@@ -21,11 +21,5 @@ def can_checkout(item_count):
 def loyalty_discount(points):
     if points < 0:
         raise ValueError("points must be >= 0")
-
-    if points >= 1000:
-        return 10
-    elif points >= 500:
-        return 5
-    else:
-        return 0
+    return 0
 
